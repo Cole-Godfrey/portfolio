43a6916048283020c782bfc2226fca3c9ab6fd36
Cole-Godfrey <colegodfrey34@gmail.com>
2025-06-23 13:10:39 -0700

    Updated navbar, added index.js - structure

---
+++ src/constants/index.js
@@ -0,0 +1,43 @@
+export const experienceData = [
+    {
+        title: "B.S. Computer Science",
+        description:
+            "I am currently pursuing a computer science degree at Washington State University. I am expected to graduate in May of 2028",
+        items: [
+            {
+                title: "GPA: 3.96",
+                description: "(4.0 excluding non-cs classes)",
+            },
+            {
+                title: "Linear Algebra, Discrete Math, Calculus III",
+                description: "(GPA: 4.0)",
+            },
+            {
+                title: "Data Structures & Algorithms - C/C++",
+                description: "(GPA: 4.0)",
+            },
+        ],
+    }
+];
+export const projects = [
+    {
+        id: 1,
+        name: "Mobile Accessories E-commerce",
+        description:
+            "An online store specializing in phone accessories including cases, chargers, cables, and power banks with MagSafe compatibility.",
+        href: "",
+        image: "/assets/projects/mobile-accessories-store.jpg",
+        bgImage: "/assets/backgrounds/blanket.jpg",
+        frameworks: [
+            { id: 1, name: "React" },
+            { id: 2, name: "Next.js" },
+            { id: 3, name: "Node.js" },
+            { id: 4, name: "MongoDB" },
+            { id: 5, name: "Tailwind CSS" },
+        ],
+    }
+];
+export const socials = [
+    { name: "LinkedIn", href: "https://www.linkedin.com/in/cole-godfrey-20b086324/" },
+    { name: "GitHub", href: "https://github.com/Cole-Godfrey/" },
+];
