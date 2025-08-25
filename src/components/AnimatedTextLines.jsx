58157a4d2156e7e082a3f41bc6bcf6d5b2b87383
Cole-Godfrey <colegodfrey34@gmail.com>
2025-08-24 20:10:06 -0700

    Added contact section and loading screen.

--- src/components/AnimatedTextLines.jsx
+++ src/components/AnimatedTextLines.jsx
@@ -1,15 +1,12 @@
-import {useRef} from "react";
-import {useGSAP} from "@gsap/react";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger)
-
-export const AnimatedTextLines = ({text, className}) => {
-    const lines = text.split('\n').filter((line) => line.trim() !== '')
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
+export const AnimatedTextLines = ({ text, className }) => {
     const containerRef = useRef(null);
     const lineRefs = useRef([]);
-
+    const lines = text.split("\n").filter((line) => line.trim() !== "");
     useGSAP(() => {
         if (lineRefs.current.length > 0) {
             gsap.from(lineRefs.current, {
@@ -20,18 +17,22 @@ export const AnimatedTextLines = ({text, className}) => {
                 ease: "back.out",
                 scrollTrigger: {
                     trigger: containerRef.current,
-                    start: "top 80%",
-                    end: "bottom 20%",
-                    toggleActions: "play none none reverse",
-                    markers: false
-                }
-            })
+                },
+            });
         }
-    })
-    return <div className={className}
-                ref={containerRef}>{lines.map((line, index) => (
-        <span key={index}
-              className="block leading-relaxed tracking-wide text-pretty"
-              ref={(el) => (lineRefs.current[index] = el)}>{line}</span>
-    ))}</div>;
-}
+    });
+
+    return (
+        <div ref={containerRef} className={className}>
+            {lines.map((line, index) => (
+                <span
+                    key={index}
+                    ref={(el) => (lineRefs.current[index] = el)}
+                    className="block leading-relaxed tracking-wide text-pretty"
+                >
+          {line}
+        </span>
+            ))}
+        </div>
+    );
+};
