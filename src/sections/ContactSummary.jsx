bad9e30d6a8bd8351bbe02010a8fa463777754fc
Cole-Godfrey <colegodfrey34@gmail.com>
2025-08-24 17:04:46 -0700

    Added GSAP animation that pins the quote to center screen for 800px

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -1,10 +1,24 @@
 import {useRef} from "react";
 import Marquee from "../components/Marquee.jsx";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const ContactSummary = () => {
     const containerRef = useRef(null);
     const items = ["Innovation", "Precision", "Collaboration", "Excellence"]
     const items2 = ["contact me", "contact me", "contact me", "contact me"]
+    useGSAP(() => {
+        gsap.to(containerRef.current, {
+            scrollTrigger: {
+                trigger: containerRef.current,
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            }
+        });
+    });
     return (
         <section ref={containerRef}
         className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16">
